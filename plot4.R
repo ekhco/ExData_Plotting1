8d7e485d65c4e85948409eb6d11009321a2bcc79
Eric Chow <echow@Eric-Chows-Mac-mini.local>
2016-12-01 17:15:14 -0500

    added R code and png for plot 4. DONE.

---
+++ plot4.R
@@ -0,0 +1,65 @@
+rm(list=ls())
+gc()
+# --------------------------------------------------
+# plot 1: make a histogram
+# copyright Eric Chow
+# --------------------------------------------------
+# install.packages("data.table")
+library(data.table)
+
+setwd("/Users/echow/Dropbox (Personal)/coursera/ExData_Plotting1")
+
+p <- fread(file="household_power_consumption.txt", sep=";", header=TRUE, na.strings=c("?"), stringsAsFactors=FALSE)
+nrow(p)
+ncol(p)
+head(p)
+
+# subset to only Feb 1-2, 2007
+p_ <- p[p$Date %in% c("1/2/2007", "2/2/2007"),]
+p_$Date <- as.Date(p_$Date, format="%d/%m/%Y")
+p_ <- data.frame(p_)
+summary(p_)
+
+
+# fix time
+d <- strptime(p_$Time, format="%H:%M:%S")
+head(d)
+p_$time <- as.numeric(d - min(d))
+# but add 24hrs to second day
+p_[p_$Date == as.Date("2/2/2007", format="%d/%m/%Y"), "time"] <- p_[p_$Date == as.Date("2/2/2007", format="%d/%m/%Y"), "time"] + 24*60*60
+summary(p_)
+
+# plot it to PNG
+png(file="plot4.png", width=480, height=480, units="px")
+	
+par(mfrow=c(2,2))
+
+	# 1,1 plot
+	plot(Global_active_power ~ time, data=p_, main="", xaxt="n",
+		ylab="Global Active Power", type="l", xlab="")
+	axis(1, at=c(0,1,2)*24*60*60, labels=c("Thu", "Fri", "Sat"))
+
+	# 1,2 plot
+	plot(Voltage ~ time, data=p_, main="", xaxt="n",
+		ylab="Voltage", type="l", xlab="datetime")
+	axis(1, at=c(0,1,2)*24*60*60, labels=c("Thu", "Fri", "Sat"))
+
+	# 2,1 plot
+	plot(Sub_metering_1 ~ time, data=p_, main="", xaxt="n",
+		ylab="Energy sub metering", type="l", xlab="", col="black")
+	lines(Sub_metering_2 ~ time, data=p_, main="", xaxt="n",
+		type="l", xlab="", col="red")
+	lines(Sub_metering_3 ~ time, data=p_, main="", xaxt="n",
+		type="l", xlab="", col="blue")
+	axis(1, at=c(0,1,2)*24*60*60, labels=c("Thu", "Fri", "Sat"))
+	legend("topright", lty=c(1,1,1), col=c("black", "red", "blue"), legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
+
+	# 2,2 plot
+	plot(Global_reactive_power ~ time, data=p_, main="", xaxt="n",
+		ylab="Global_reactive_power", type="l", xlab="datetime")
+	axis(1, at=c(0,1,2)*24*60*60, labels=c("Thu", "Fri", "Sat"))
+
+dev.off()
+
+
+#              ~ fin ~
